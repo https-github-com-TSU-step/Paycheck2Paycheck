8d0e9d614e3316bd842267d3dfc64cb9c029dfa5
Stepan_Bazh <bazhenov.stepan1@gmail.com>
2026-03-09 21:56:46 +0700

    Создал класс UseCase-а AddExpenseManually и написал интерфейсы в repository

---
+++ app/src/main/java/com/example/paycheck2paycheck/domain/repository/ExpenseRepository.kt
@@ -0,0 +1,9 @@
+package com.example.paycheck2paycheck.domain.repository
+
+import com.example.paycheck2paycheck.domain.model.Expense
+
+interface ExpenseRepository {
+    suspend fun getExpenseById(id: String): Expense?
+
+    suspend fun addExpense(expense: Expense)
+}
